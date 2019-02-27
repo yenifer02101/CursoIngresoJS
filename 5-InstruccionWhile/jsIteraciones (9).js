9c415792630a5a4dd43e305099571788ae3d7e3f
yenifer02101 <yenny_1892@hotmail.com>
2019-02-27 12:01:33 -0300

    clase02/27

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -8,9 +8,25 @@ function mostrar()
 
 	while(respuesta!='no')
 	{
+		var nota;
+		nota=prompt("");
+		nota=parseint(nota);
+		while( isNaN(nota==true||<0 || nota>10)
+		{
+			nota= prompt("error");
+			nota= parseinte(nota);
+
+		}
+		alert(nota);
 		
-	
-	}
+		var sexo;
+		sexo=prompt("");
+		while(isNaN(sexo)==false||sexo!="f"&&sexo!="m")
+		{
+			sexo= prompt(error);
+		}	
+	}alert(sexo);
+	respuesta=prompt("continuar");
 
 
 
